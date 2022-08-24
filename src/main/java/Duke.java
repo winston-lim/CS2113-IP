c12db266dae2869d298bf3bd30fe48cfcbfab684
winston-lim <winston_lim1@hotmail.com>
2022-08-24 11:34:07 +0800

    Update: add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+
         // print initial greeting
         System.out.println("------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("------------------------------");
+
+        // get user input
+        String command = ss.nextLine();
+        while (!command.equals("exit")) {
+            System.out.println("    ------------------------------");
+            System.out.println("    " + command);
+            System.out.println("    ------------------------------");
+            command = ss.nextLine();
+        }
 
         // print final greeting
         System.out.println("------------------------------");
