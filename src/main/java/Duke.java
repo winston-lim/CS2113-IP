447df86e0eb0c234a2c501ffc45982d0ea10dda2
winston-lim <winston_lim1@hotmail.com>
2022-08-24 11:33:20 +0800

    Refactor: add comments and remove unncessary line breaks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("------------------------------\n");
-        System.out.println("Hello! I'm Duke\n");
-        System.out.println("What can I do for you?\n");
-        System.out.println("------------------------------\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("------------------------------\n");
+        // print initial greeting
+        System.out.println("------------------------------");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("------------------------------");
+
+        // print final greeting
+        System.out.println("------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------");
     }
 }
