d5dc9461cb2be6b5906d06a9f70ebabc950e68e7
winston-lim <winston_lim1@hotmail.com>
2022-08-25 15:08:12 +0800

    Feature: add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+}
