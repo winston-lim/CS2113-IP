7e78f73d2b87d38208c8212ab5831b5743c92794
winston-lim <winston_lim1@hotmail.com>
2022-08-25 15:08:54 +0800

    Update: mark and unmark functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,38 +3,79 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
+    static final String LINE_SEPERATOR = "------------------------------";
+    static final String RESPONSE_INDENTATION = "    ";
+
     public static void main(String[] args) {
         // intialize variables
         Scanner ss = new Scanner(System.in);
-        List<String> recordedCommands = new ArrayList<String>();
+        TaskManager taskManager = new TaskManager();
 
-        // print initial greeting
-        System.out.println("------------------------------");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("------------------------------");
+        printIntialGreeting();
 
         // get user input
-        String command = ss.nextLine();
-        while (!command.equals("bye")) {
-            if (command.equals("list")) { // list commands
-                System.out.println("    ------------------------------");
-                for (int i = 0; i < recordedCommands.size(); ++i) {
-                    System.out.println("    " + (i + 1) + ". " + recordedCommands.get(i));
+        String input = ss.nextLine();
+
+        // maintain conversation
+        while (!input.equals("bye")) {
+            String[] inputs = input.split(" ");
+            List<String> outputs = new ArrayList<String>();
+            if (inputs[0].equals("list")) { // list inputs
+                List<Task> recordedTasks = taskManager.getRecordedTasks();
+                if (recordedTasks.size() == 0)
+                    outputs.add("No recorded tasks");
+                for (int i = 0; i < taskManager.getTaskCount(); ++i) {
+                    outputs.add((i + 1) + ". " + "[" + recordedTasks.get(i).getStatusIcon() + "] "
+                            + recordedTasks.get(i).getDescription());
                 }
-                System.out.println("    ------------------------------");
-            } else { // add commands to record
-                recordedCommands.add(command);
-                System.out.println("    ------------------------------");
-                System.out.println("    added " + command);
-                System.out.println("    ------------------------------");
+            } else if (inputs[0].equals("mark")) {
+                if (inputs.length == 1) {
+                    outputs.add("Error: Task number is required");
+                } else {
+                    outputs.add(taskManager.markTask(Integer.parseInt(inputs[1])));
+                }
+            } else if (inputs[0].equals("unmark")) {
+                if (inputs.length == 1) {
+                    outputs.add("Error: Task number is required");
+                } else {
+                    outputs.add(taskManager.unmarkTask(Integer.parseInt(inputs[1])));
+                }
+            } else { // add inputs to record
+                if (inputs[0].length() != 0) {
+                    outputs.add(taskManager.addTask(inputs[0]));
+                }
             }
-            command = ss.nextLine();
+            printResponse(outputs);
+            outputs.clear();
+            input = ss.nextLine();
         }
 
-        // print final greeting
-        System.out.println("------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------");
+        ss.close();
+
+        printFinalGreeting();
     }
+
+    public static void printIntialGreeting() {
+        System.out.println(LINE_SEPERATOR);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE_SEPERATOR);
+    }
+
+    public static void printFinalGreeting() {
+        System.out.println(LINE_SEPERATOR);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE_SEPERATOR);
+    }
+
+    public static void printResponse(List<String> responses) {
+        if (responses.size() != 0) {
+            System.out.println(RESPONSE_INDENTATION + LINE_SEPERATOR);
+            for (int i = 0; i < responses.size(); ++i) {
+                System.out.println(RESPONSE_INDENTATION + responses.get(i));
+            }
+            System.out.println(RESPONSE_INDENTATION + LINE_SEPERATOR);
+        }
+    }
+
 }
