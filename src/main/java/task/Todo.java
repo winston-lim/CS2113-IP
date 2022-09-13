6baea3bca637b6d97dc090819634938d10fa648c
winston-lim <winston_lim1@hotmail.com>
2022-09-13 16:57:23 +0800

    Remove id from Task and add overloaded constructor

--- src/main/java/task/Todo.java
+++ src/main/java/task/Todo.java
@@ -3,8 +3,12 @@ package task;
 public class Todo extends Task {
     private static final String TODO_PREFIX = "[T] ";
 
-    Todo(String description, int id) {
-        super(description, id);
+    Todo(String description) {
+        super(description);
+    }
+
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
     }
 
     @Override
